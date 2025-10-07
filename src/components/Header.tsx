919fa96105f1f94ab6fbeed7f30d5bca9719c174
Dennis <miriye4@gmail.com>
2025-10-07 15:43:49 +0200

    Increase logo size by 50% - now 144x144px for maximum brand impact

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,13 +16,13 @@ export const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 glass border-b border-border/50">
       <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-28">
+        <div className="flex items-center justify-between h-40">
           {/* Logo */}
           <div className="flex items-center space-x-3 flex-shrink-0">
             <img 
               src={synkroLogo} 
               alt="Synkro Logo" 
-              className="h-24 w-24 object-contain"
+              className="h-36 w-36 object-contain"
             />
           </div>
 
